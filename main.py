ee10230c7a89ebb64ea14469b394fc13208312f1
le-grand-mannitout <ouaksel.madil@gmail.com>
2021-02-14 21:44:13 +0100

    Add test of capacity to move according the board dimension + support to translate chess x coordinate (str) to int + add documentation to functions

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
 
+def is_moved(piece, x, y):
+    """
+        Determine if a piece can be move according to the
+        type of piece and the board dimension
+    """
+    x = translate_coordinates(x)
+    
+    return (piece.mov(x, y) and
+            board_limit(x, y))
+
+
+def translate_coordinates(x):
+    """
+        Translate chess x coordinate (str) to int 
+    """
+    try :
+        LETTER_COORDINATES = ("abcdefgh")
+        
+        return LETTER_COORDINATES.index(x) + 1    
+ 
+    except TypeError:
+        return x
+
+
+def board_limit(x, y):
+    """
+        Determine if move coordinates are included in the chess board
+    """
+    return ((x > 0 and x <= 8) and
+            (y > 0 and y <= 8))
+
+
 class Pawn:
     
     def __init__(self, x_pos, y_pos):
@@ -6,7 +38,10 @@ class Pawn:
         self.y_pos = y_pos
         
     def mov(self, x, y):
-    
+        """
+            Determine if movement is correct according to the
+            pawn allowed moves
+        """
         return (x - self.x_pos == 0 and
                 y - self.y_pos == 1)
 
@@ -18,7 +53,10 @@ class Bishop:
         self.y_pos = y_pos
         
     def mov(self, x, y):
-    
+        """
+            Determine if movement is correct according to the
+            bishop allowed moves
+        """
         return (abs(x - self.x_pos) == abs(y - self.y_pos) and
                 x - self.x_pos != 0)
 
@@ -30,7 +68,10 @@ class Knight:
         self.y_pos = y_pos
         
     def mov(self, x, y):
-        
+        """
+            Determine if movement is correct according to
+            knight allowed moves
+        """
         return ((abs(x - self.x_pos) == 2 and
                 abs(y - self.y_pos) == 1) or
                 (abs(x - self.x_pos) == 1 and
@@ -44,7 +85,10 @@ class Rook:
         self.y_pos = y_pos
         
     def mov(self, x, y):
-        
+        """
+            Determine if movement is correct according to
+            rook allowed moves
+        """
         return ((x - self.x_pos != 0 and
                 y - self.y_pos == 0) or
                 (x - self.x_pos == 0 and
@@ -58,7 +102,10 @@ class Queen:
         self.y_pos = y_pos
         
     def mov(self, x, y):
-        
+        """
+            Determine if movement is correct according to
+            queen allowed moves
+        """
         return ((abs(x - self.x_pos) == abs(y - self.y_pos) and
                 x - self.x_pos != 0) or
                 ((x - self.x_pos != 0 and
@@ -74,6 +121,9 @@ class King:
         self.y_pos = y_pos
         
     def mov(self, x, y):
-        
-        return ((abs(x - self.x_pos) == 1 or
-                abs(y - self.y_pos) == 1))
+        """
+            Determine if movement is correct according to
+            king allowed moves
+        """
+        return (abs(x - self.x_pos) == 1 or
+                abs(y - self.y_pos) == 1)
